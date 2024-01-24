61d29a390346fd5d70aa07964639e2a9964d3c2e
WillianCosta <willian95@outlook.com>
2024-01-24 18:51:14 -0300

    feat: adição de lógica para exibir informações de usuário dinamicamente

--- frontend/front/src/app/screens/profile/profile.component.ts
+++ frontend/front/src/app/screens/profile/profile.component.ts
@@ -7,32 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent {
+  //controle de dados do usuário
+  user = {
+    name: 'Camila Soares',
+    locale: 'Brasil',
+    profileImg: 'assets/imgs/img_profile_orange_portfolio.png',
+  };
+
   //Array para projetos
-  projects: IProjects[] = [  {
-    title: 'teste',
-    tags: ['ux', 'ui'],
-    link: 'https://github.com/costaowillian/cashquest',
-    description: 'teste, teste, teste',
-    img: 'assets/imgs/img_landingpage_orange_portfolio.svg',
-    releaseDate: '12/10'
-  },
-  {
-    title: 'teste1',
-    tags: ['ux', 'ui'],
-    link: 'https://github.com/costaowillian/cashquest',
-    description: 'teste, teste, teste',
-    img: 'assets/imgs/img_landingpage_orange_portfolio.svg',
-    releaseDate: '12/10'
-  },
-  {
-    title: 'teste2',
-    tags: ['ux', 'ui'],
-    link: 'https://github.com/costaowillian/cashquest',
-    description: 'teste, teste, teste',
-    img: 'assets/imgs/img_landingpage_orange_portfolio.svg',
-    releaseDate: '12/10'
-  }
-];
+  projects: IProjects[] = [
+    {
+      title: 'teste',
+      tags: ['ux', 'ui'],
+      link: 'https://github.com/costaowillian/cashquest',
+      description: 'teste, teste, teste',
+      img: 'assets/imgs/img_landingpage_orange_portfolio.svg',
+      releaseDate: '12/10',
+    },
+    {
+      title: 'teste1',
+      tags: ['ux', 'ui'],
+      link: 'https://github.com/costaowillian/cashquest',
+      description: 'teste, teste, teste',
+      img: 'assets/imgs/img_landingpage_orange_portfolio.svg',
+      releaseDate: '12/10',
+    },
+    {
+      title: 'teste2',
+      tags: ['ux', 'ui'],
+      link: 'https://github.com/costaowillian/cashquest',
+      description: 'teste, teste, teste',
+      img: 'assets/imgs/img_landingpage_orange_portfolio.svg',
+      releaseDate: '12/10',
+    },
+  ];
 
   constructor() {}
 }
