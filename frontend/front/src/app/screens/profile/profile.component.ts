e39fabba65b48efe28824579cdcf7830e8a42236
WillianCosta <willian95@outlook.com>
2024-01-24 20:11:03 -0300

    feat: adicionado sistema de pesquisa na tela de perfil

--- frontend/front/src/app/screens/profile/profile.component.ts
+++ frontend/front/src/app/screens/profile/profile.component.ts
@@ -1,7 +1,13 @@
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 import { IProjects } from './models/project';
 import { Component, OnInit } from '@angular/core';
-import { debounceTime, distinctUntilChanged, tap } from 'rxjs';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  map,
+  switchMap
+} from 'rxjs';
 
 @Component({
   selector: 'app-profile',
@@ -21,11 +27,14 @@ export class ProfileComponent implements OnInit {
     profileImg: 'assets/imgs/img_profile_orange_portfolio.png',
   };
 
+  //controle de dados pesquisados
+  searchProjects: IProjects[] = [];
+
   //Array para projetos
   projects: IProjects[] = [
     {
       title: 'teste',
-      tags: ['ux', 'ui'],
+      tags: ['design', 'ui'],
       link: 'https://github.com/costaowillian/cashquest',
       description: 'teste, teste, teste',
       img: 'assets/imgs/img_landingpage_orange_portfolio.svg',
@@ -33,7 +42,7 @@ export class ProfileComponent implements OnInit {
     },
     {
       title: 'teste1',
-      tags: ['ux', 'ui'],
+      tags: ['teste', 'figma', 'photoshop'],
       link: 'https://github.com/costaowillian/cashquest',
       description: 'teste, teste, teste',
       img: 'assets/imgs/img_landingpage_orange_portfolio.svg',
@@ -41,7 +50,7 @@ export class ProfileComponent implements OnInit {
     },
     {
       title: 'teste2',
-      tags: ['ux', 'ui'],
+      tags: ['c++'],
       link: 'https://github.com/costaowillian/cashquest',
       description: 'teste, teste, teste',
       img: 'assets/imgs/img_landingpage_orange_portfolio.svg',
@@ -51,6 +60,28 @@ export class ProfileComponent implements OnInit {
 
   constructor(private formBuilder: FormBuilder) {}
   ngOnInit(): void {
+    this.searchForm
+      .get('search')
+      ?.valueChanges.pipe(
+        map((value) => value!.trim()),
+        filter((value) => value.length > 1),
+        debounceTime(300),
+        distinctUntilChanged(),
+        //tap((value => console.log(value)))),
+        switchMap(async (value) => this.handleSearch(value))
+      )
+      .subscribe();
+  }
 
+  handleSearch(value: string) {
+    console.log(value);
+    for (let projetc of this.projects) {
+      for (let i = 0; i <= projetc.tags!.length; i++) {
+        if (projetc.tags![i] == value) {
+          this.searchProjects.push(projetc);
+          console.log(this.searchProjects);
+        }
+      }
+    }
   }
 }
