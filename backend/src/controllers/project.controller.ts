80c1b1e6363296a13d747214d7aa74fb053ca52d
Gustavo Medeiros <gugamedeiros517@gmail.com>
2024-01-25 22:20:36 -0300

    :sparkles: feat: Adiciona método de adicionar no controller de project.

--- backend/src/controllers/project.controller.ts
+++ backend/src/controllers/project.controller.ts
@@ -2,6 +2,15 @@ import ProjectService from "../services/project.service";
 import { Request, Response } from "express";
 
 class ProjectController {
+  public static async createProject(req: Request, res: Response) {
+    const project = req.body;
+    const projectId = await ProjectService.createProject(project);
+
+    if (!projectId) return res.status(500);
+
+    return res.status(201).json({ projectId });
+  }
+
   public static async getAllProjects(_req: Request, res: Response) {
     const projects = await ProjectService.getAllProjects();
 
