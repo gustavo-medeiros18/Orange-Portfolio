90781a3fbbf021c27c796b27a98a103da4072640
Gustavo Medeiros <gugamedeiros517@gmail.com>
2024-01-25 22:21:04 -0300

    :sparkles: feat: Adiciona endpoint para adicionar project no router.

--- backend/src/routers/project.router.ts
+++ backend/src/routers/project.router.ts
@@ -5,5 +5,6 @@ const projectRouter = Router();
 
 projectRouter.get("/projects", ProjectController.getAllProjects);
 projectRouter.get("/projects/user/:userId", ProjectController.getAllProjectsByUserId);
+projectRouter.post("/projects", ProjectController.createProject);
 
 export default projectRouter;
