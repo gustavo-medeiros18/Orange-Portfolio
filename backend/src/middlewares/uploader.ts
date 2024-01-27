25cec653a1fa214a2c915341852ae4dda841eee5
Gustavo Medeiros <gugamedeiros517@gmail.com>
2024-01-26 21:39:17 -0300

    :sparkles: feat: Adiciona verificação da existência do diretório de uploads, a fim de criá-lo se necessário.

--- backend/src/middlewares/uploader.ts
+++ backend/src/middlewares/uploader.ts
@@ -1,13 +1,23 @@
 import multer from "multer";
+import fs from "fs";
+
+const uploadDir = "./uploads";
+if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir);
 
 const storage = multer.diskStorage({
   destination: function (_req, _file, cb) {
-    cb(null, "./uploads");
+    const uploadDir = "./uploads";
+    if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir);
+
+    cb(null, uploadDir);
   },
   filename: function (_req, file, cb) {
-    cb(null, file.fieldname + "-" + Date.now());
+    const mimetype = file.mimetype.split("/")[1];
+    const uploadedFileName = `${file.fieldname}-${Date.now()}.${mimetype}`;
+
+    cb(null, uploadedFileName);
   },
 });
 
 const upload = multer({ storage: storage });
-export const imageUploader = upload.single("file");
+export const imageUploader = upload.single("image");
