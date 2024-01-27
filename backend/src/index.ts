a0f8ddcc7a53fb3247b0b86a6d1874d1f112e6d2
Gustavo Medeiros <gugamedeiros517@gmail.com>
2024-01-27 13:48:39 -0300

    :recycle: refact: Altera o nome do middleware do multer.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,14 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import userRouter from "./routers/user.router";
 import projectRouter from "./routers/project.router";
-import { imageUploader } from "./middlewares/uploader";
+import { multerMiddleware } from "./middlewares/uploader";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
-app.use(imageUploader);
+app.use(multerMiddleware);
 
 app.listen(process.env.PORT, () => console.log(`Express rodando na porta ${process.env.PORT}`));
 
