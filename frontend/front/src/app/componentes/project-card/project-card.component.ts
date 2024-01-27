72a207c2c9647ca1bc1b8dd492e7f78afe46e03a
WillianCosta <willian95@outlook.com>
2024-01-27 16:44:57 -0300

    feat: adição de validação para deletar no project card

--- frontend/front/src/app/componentes/project-card/project-card.component.ts
+++ frontend/front/src/app/componentes/project-card/project-card.component.ts
@@ -1,20 +1,24 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 import { IProject, ProjecEventEnum } from "src/app/models/iProject";
 import { ModalActionService } from "../modal-action/services/modal-action.service";
 import { DeleteConfirmationService } from "../delete-confirmation/services/delete-confirmation.service";
+import { ProjectService } from "src/app/appServices/project.service";
 
 @Component({
   selector: "app-project-card",
   templateUrl: "./project-card.component.html",
   styleUrls: ["./project-card.component.scss"],
 })
-export class ProjectCardComponent {
+export class ProjectCardComponent implements OnInit {
   @Input() projects: IProject[] = [];
   @Input() userName: string = "";
   @Input() userImg: string = "";
 
-  constructor(private modalActionService: ModalActionService,
-    private modalDeleteService: DeleteConfirmationService) {}
+  constructor(
+    private modalActionService: ModalActionService,
+    private modalDeleteService: DeleteConfirmationService
+  ) {}
+  ngOnInit(): void {}
 
   openDialog(name: string) {
     this.modalActionService.openDialog(name);
@@ -30,5 +34,12 @@ export class ProjectCardComponent {
 
   deleteProject(id: number) {
     this.modalDeleteService.openDialog();
+    this.modalDeleteService.confirm().subscribe((confirm) => {
+      if (confirm) {
+        console.log("deleta");
+      } else {
+        console.log("não deleta");
+      }
+    });
   }
 }
