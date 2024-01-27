60cdb8b7765cd037abfd628a2ea78b3009c9b3f5
WillianCosta <willian95@outlook.com>
2024-01-27 17:03:07 -0300

    refactor: remoção de botões do app component

--- frontend/front/src/app/app.component.ts
+++ frontend/front/src/app/app.component.ts
@@ -1,28 +1,18 @@
-import { Component} from '@angular/core';
-import { ProjectActionService } from './componentes/project-action/services/project-action.service';
-import { DeleteConfirmationService } from './componentes/delete-confirmation/services/delete-confirmation.service';
-import { ViewProjectInfoService } from './componentes/view-project-info/services/view-project-info.service';
-
+import { Component } from "@angular/core";
+import { ProjectActionService } from "./componentes/project-action/services/project-action.service";
+import { DeleteConfirmationService } from "./componentes/delete-confirmation/services/delete-confirmation.service";
+import { ViewProjectInfoService } from "./componentes/view-project-info/services/view-project-info.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'front';
-  constructor(private projectActionService: ProjectActionService, private deleteConfirmationService: DeleteConfirmationService,
-    private viewProjectInfoService: ViewProjectInfoService) {}
+  title = "front";
+  constructor(private viewProjectInfoService: ViewProjectInfoService) {}
 
-  openDialog(action: string,result: string) {
-    this.projectActionService.openDialog(action,result);
-  }
-
-  openDialogDelete() {
-    this.deleteConfirmationService.openDialog();
-  }
-
-  openDialogViewProject(data: string){
+  openDialogViewProject(data: string) {
     this.viewProjectInfoService.openDialog(data);
   }
 }
