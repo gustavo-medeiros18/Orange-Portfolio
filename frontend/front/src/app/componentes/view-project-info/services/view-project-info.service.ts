715916eb7be5ea242ddd13eb8d4502a57c7e62a9
pedroaugusto04 <pedroaugustoaduarte@gmail.com>
2024-01-27 15:14:07 -0300

    feat: adicionado protótipo da aba de visualização de um projeto no desktop

--- frontend/front/src/app/componentes/view-project-info/services/view-project-info.service.ts
+++ frontend/front/src/app/componentes/view-project-info/services/view-project-info.service.ts
@@ -10,16 +10,15 @@ export class ViewProjectInfoService {
   constructor(private dialog: MatDialog) { }
 
   openDialog(data: string) {
-    const isMobile = window.innerWidth <= 992;
-    const width = isMobile ? "22.5rem" : "65.125rem";
-    const height = isMobile ? "43.625rem" : "61.30094rem";
-    const position = isMobile ? { top: "9.25rem" } : { top: "8.38rem", bottom: "15rem" };
   
     const dialogRef = this.dialog.open(ViewProjectInfoComponent, {
       data: { data: data },
-      position: position,
-      width: width,
-      height: height,
+      width: "100rem",
+      height: "45rem",
+      position: { 
+        top: "8.37rem",
+        bottom: "1.7rem" 
+      },
     });
   }
 }
