9303d2d944018a87993a225ef7663aeb73aaf18b
Gustavo Medeiros <gugamedeiros517@gmail.com>
2024-01-26 21:40:41 -0300

    :sparkles: feat: Adiciona método de criar um project (com upload de imagem) no controller.

--- backend/src/controllers/project.controller.ts
+++ backend/src/controllers/project.controller.ts
@@ -1,12 +1,29 @@
 import ProjectService from "../services/project.service";
 import { Request, Response } from "express";
 import multer from "multer";
+import { Project } from "../models/project.model";
 
 class ProjectController {
   public static async createProject(req: Request, res: Response) {
-    console.log(req.file);
+    const newProject: Project = req.body;
 
-    res.send("ok");
+    if (
+      !newProject ||
+      !newProject.title ||
+      !newProject.description ||
+      !newProject.tags ||
+      !newProject.link ||
+      !req.file
+    ) {
+      return res
+        .status(422)
+        .json({ message: "Solicitação inválida. Verifique os parâmetros enviados." });
+    }
+    newProject.img_url = req.file!.path;
+
+    const createdProject = await ProjectService.createProject(newProject);
+
+    return res.status(201).json(createdProject);
   }
 
   public static async getAllProjects(_req: Request, res: Response) {
