e187cbbae4c2a32bbc6e5f65ef61e8b68dd9483c
WillianCosta <willian95@outlook.com>
2024-01-27 13:56:17 -0300

    feat:criação de componente para tela de descobrir

---
+++ frontend/front/src/app/screens/discover/discover.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppRoutingModule } from './discover-routing.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AppRoutingModule
+  ]
+})
+export class DiscoverModule { }
