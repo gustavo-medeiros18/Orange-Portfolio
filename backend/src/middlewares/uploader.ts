f864c26185ccac16ad3ae33f499037bfbfecd07d
Gustavo Medeiros <gugamedeiros517@gmail.com>
2024-01-27 11:51:17 -0300

    :sparkles: feat: Altera o uploader para subir o arquivo para o firebase.

--- backend/src/middlewares/uploader.ts
+++ backend/src/middlewares/uploader.ts
@@ -1,21 +1,40 @@
 import multer from "multer";
 import fs from "fs";
+import { initializeApp } from "firebase/app";
+import { getStorage, ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
+import { firebaseConfig } from "../config/firebase.config";
 
 const uploadDir = "./uploads";
 if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir);
 
 const storage = multer.diskStorage({
-  destination: function (_req, _file, cb) {
+  destination: (_req, _file, cb) => {
     const uploadDir = "./uploads";
     if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir);
 
     cb(null, uploadDir);
   },
-  filename: function (_req, file, cb) {
+  filename: async function uploadToFirebase(_req, file, cb) {
     const mimetype = file.mimetype.split("/")[1];
     const uploadedFileName = `${file.fieldname}-${Date.now()}.${mimetype}`;
 
     cb(null, uploadedFileName);
+
+    const firebaseApp = initializeApp(firebaseConfig);
+
+    const storage = getStorage(firebaseApp);
+    const storageRef = ref(storage, uploadedFileName);
+
+    const metadata = {
+      contentType: file.mimetype,
+    };
+
+    const snapshot = await uploadBytesResumable(storageRef, file.buffer, metadata);
+    const downloadURL = await getDownloadURL(snapshot.ref);
+
+    console.log("Uploaded a blob or file!", downloadURL);
+
+    cb(null, downloadURL);
   },
 });
 
