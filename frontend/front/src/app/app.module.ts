8ef8707990f42e9220647f98f799fbf2bf1d0341
WillianCosta <willian95@outlook.com>
2024-01-29 14:02:22 -0300

    feat: criação de mobile view component

--- frontend/front/src/app/app.module.ts
+++ frontend/front/src/app/app.module.ts
@@ -1,33 +1,34 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { LoginComponent } from './screens/login/login.component';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { RegisterComponent } from './screens/register/register.component';
-import { ProfileComponent } from './screens/profile/profile.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { HttpClientModule } from '@angular/common/http';
-import { NavBarComponent } from './componentes/nav-bar/nav-bar.component';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatCardModule } from '@angular/material/card';
-import { ProjectCardComponent } from './componentes/project-card/project-card.component';
-import { MatChipsModule } from '@angular/material/chips';
-import { ModalActionComponent } from './componentes/modal-action/modal-action.component';
-import { MatDialogModule } from '@angular/material/dialog';
-import {MatMenuModule} from '@angular/material/menu';
-import { ProjectActionComponent } from './componentes/project-action/project-action.component';
-import { DeleteConfirmationComponent } from './componentes/delete-confirmation/delete-confirmation.component';
-import { ViewProjectInfoComponent } from './componentes/view-project-info/view-project-info.component';
-import { DiscoverComponent } from './screens/discover/discover.component';
-import { DisplayProjectComponent } from './componentes/display-project/display-project.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { MatInputModule } from "@angular/material/input";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { LoginComponent } from "./screens/login/login.component";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { RegisterComponent } from "./screens/register/register.component";
+import { ProfileComponent } from "./screens/profile/profile.component";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { HttpClientModule } from "@angular/common/http";
+import { NavBarComponent } from "./componentes/nav-bar/nav-bar.component";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatCardModule } from "@angular/material/card";
+import { ProjectCardComponent } from "./componentes/project-card/project-card.component";
+import { MatChipsModule } from "@angular/material/chips";
+import { ModalActionComponent } from "./componentes/modal-action/modal-action.component";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatMenuModule } from "@angular/material/menu";
+import { ProjectActionComponent } from "./componentes/project-action/project-action.component";
+import { DeleteConfirmationComponent } from "./componentes/delete-confirmation/delete-confirmation.component";
+import { ViewProjectInfoComponent } from "./componentes/view-project-info/view-project-info.component";
+import { DiscoverComponent } from "./screens/discover/discover.component";
+import { DisplayProjectComponent } from "./componentes/display-project/display-project.component";
+import { MobileViewProjectComponent } from "./componentes/mobil-view-project/mobile-view-project.component";
 
 @NgModule({
   declarations: [
@@ -42,7 +43,8 @@ import { DisplayProjectComponent } from './componentes/display-project/display-p
     DeleteConfirmationComponent,
     ViewProjectInfoComponent,
     DiscoverComponent,
-    DisplayProjectComponent
+    DisplayProjectComponent,
+    MobileViewProjectComponent,
   ],
   imports: [
     BrowserModule,
@@ -61,7 +63,7 @@ import { DisplayProjectComponent } from './componentes/display-project/display-p
     MatChipsModule,
     MatDialogModule,
     FormsModule,
-    MatMenuModule
+    MatMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ frontend/front/src/app/componentes/mobil-view-project/mobile-view-project.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mobil-view-project',
+  templateUrl: './mobile-view-project.component.html',
+  styleUrls: ['./mobile-view-project.component.scss']
+})
+export class MobileViewProjectComponent {
+
+}
