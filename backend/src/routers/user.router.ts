f5d9c0e91eeb1d17af8a2e278fee509cdf14c690
pedroaugusto04 <pedroaugustoaduarte@gmail.com>
2024-01-29 18:08:01 -0300

    chore: configuracoes e ajustes  para o  primeiro deploy

--- backend/src/routers/user.router.ts
+++ backend/src/routers/user.router.ts
@@ -6,7 +6,7 @@ const userRouter = Router();
 
 userRouter.post("/users", UserController.createUser);
 
-userRouter.get("/users", authenticateMiddleware, UserController.getAllUsers);
+userRouter.get("/users", UserController.getAllUsers);
 userRouter.get("/users/:id", authenticateMiddleware, UserController.getUserById);
 userRouter.delete("/users/:id", authenticateMiddleware, UserController.deleteUser);
 userRouter.put("/users/:id", authenticateMiddleware, UserController.updateUser);
