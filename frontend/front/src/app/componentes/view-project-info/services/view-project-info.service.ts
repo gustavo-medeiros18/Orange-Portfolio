5fa23f0428df49b7f2df6f65eb3c3360e2584304
WillianCosta <willian95@outlook.com>
2024-01-29 14:47:29 -0300

    fix: correção estilização view project

--- frontend/front/src/app/componentes/view-project-info/services/view-project-info.service.ts
+++ frontend/front/src/app/componentes/view-project-info/services/view-project-info.service.ts
@@ -38,28 +38,4 @@ export class ViewProjectInfoService {
     }
   }
 
-
-  openDialogDiscover(user: IModal, project: IProject) {
-    const isMobile = window.innerWidth < 600;
-    if (isMobile) {
-      this.dialog.open(ViewProjectInfoComponent, {
-        data: { user, project, isMobile },
-        maxWidth: "100vw",
-        maxHeight: "100vh",
-        width: "100%",
-        height: "100%",
-        panelClass: "full-screen-modal",
-      });
-    } else {
-      this.dialog.open(ViewProjectInfoComponent, {
-        data: { user, project, isMobile },
-        width: "100%",
-        height: "50rem",
-        position: {
-          top: "8.37rem",
-          bottom: "1.7rem",
-        },
-      });
-    }
-  }
 }
