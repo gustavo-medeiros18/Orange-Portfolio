e17ae697bd3f5c47cf35da9ae8018af5e1e695cb
Eriberto Lima <eribertosflima@gmail.com>
2024-01-29 13:12:58 -0300

    refact: altera forma de utilização do middleware de autenticação na rota de usuarios e modifica a variavel de retorno da controller de createUser

--- backend/src/routers/user.router.ts
+++ backend/src/routers/user.router.ts
@@ -6,11 +6,9 @@ const userRouter = Router();
 
 userRouter.post("/users", UserController.createUser);
 
-userRouter.use(authenticateMiddleware);
-
-userRouter.get("/users", UserController.getAllUsers);
-userRouter.get("/users/:id", UserController.getUserById);
-userRouter.delete("/users/:id", UserController.deleteUser);
-userRouter.put("/users/:id", UserController.updateUser);
+userRouter.get("/users", authenticateMiddleware, UserController.getAllUsers);
+userRouter.get("/users/:id", authenticateMiddleware, UserController.getUserById);
+userRouter.delete("/users/:id", authenticateMiddleware, UserController.deleteUser);
+userRouter.put("/users/:id", authenticateMiddleware, UserController.updateUser);
 
 export default userRouter;
