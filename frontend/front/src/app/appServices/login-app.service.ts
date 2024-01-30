df3e69ac92668f95051f77e010ea98c66b4f34f5
WillianCosta <willian95@outlook.com>
2024-01-30 13:49:02 -0300

    chore: instalação do @types/crypto-js

--- frontend/front/src/app/appServices/login-app.service.ts
+++ frontend/front/src/app/appServices/login-app.service.ts
@@ -2,8 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { environment } from "src/environments/environment.development";
-import * as CryptoJS from 'crypto-js';
-
+import * as CryptoJS from "crypto-js";
 
 @Injectable({
   providedIn: "root",
