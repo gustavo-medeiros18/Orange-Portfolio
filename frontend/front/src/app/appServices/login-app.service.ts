68296a9faf475829f9254cb3ec8a71037a575927
WillianCosta <willian95@outlook.com>
2024-01-30 13:35:35 -0300

    feat: adição de metodo para verificar se o usuário está logado no login service

--- frontend/front/src/app/appServices/login-app.service.ts
+++ frontend/front/src/app/appServices/login-app.service.ts
@@ -1,24 +1,32 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment.development';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { environment } from "src/environments/environment.development";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class LoginAppService {
-
   private readonly API = environment.baseUrl;
 
   constructor(private httpClient: HttpClient) {}
 
   signOut = () => {
     localStorage.removeItem("token");
-  }
+  };
 
   loginWithGoogle(credentials: string): Observable<any> {
-    const apiUrl = new URL(environment.apiLoginGoogle,this.API);
+    const apiUrl = new URL(environment.apiLoginGoogle, this.API);
     const requestBody = JSON.stringify(credentials);
-    return this.httpClient.post(environment.apiLoginGoogle,requestBody);
+    return this.httpClient.post(environment.apiLoginGoogle, requestBody);
+  }
+
+  isUserLoggedIn() {
+    const token = this.getAuthorizationToken("token");
+    if(!token) {
+      return false;
+    }
+
+    return true;
   }
 }
