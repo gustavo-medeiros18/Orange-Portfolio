221c434fa7fe9d1363d1d6d96da8763044e3c17b
WillianCosta <willian95@outlook.com>
2024-01-30 13:31:00 -0300

    feat: adição de canActivate no guard

--- frontend/front/src/app/guard/auth.guard.ts
+++ frontend/front/src/app/guard/auth.guard.ts
@@ -1,5 +1,20 @@
-import { CanActivateFn } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, CanActivateFn, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { LoginAppService } from '../appServices/login-app.service';
+import { Observable } from 'rxjs';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+@Injectable({
+  providedIn: 'root',
+})
+export class GuardGuard implements CanActivate {
+  constructor(private router: Router, private loginAppService: LoginAppService) {}
+
+  canActivate( next: ActivatedRouteSnapshot, state: RouterStateSnapshot) : boolean {
+    if(this.loginAppService.isUserLoggedIn()) {
+      return true;
+    } else {
+      this.router.navigate(['login']);
+      return false;
+    }
+  }
+}
