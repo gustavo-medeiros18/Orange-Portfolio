e5fda4e64a109c515ea5f5ae023d8b8e9e839e7d
WillianCosta <willian95@outlook.com>
2024-02-01 17:17:23 -0300

    feat: adição de validators para senha

---
+++ frontend/front/src/app/screens/Validators/validators.ts
@@ -0,0 +1,17 @@
+import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
+
+export function createPassword(): ValidatorFn {
+  return (control: AbstractControl) : ValidationErrors | null => {
+    const value =  control.value;
+    if(!value) {
+      return null;
+    }
+    // password must be at least 8 characters long and contain a number, one upper
+    const hasUppercaseCharacters = /[A-Z]+/.test(value);
+    const hasLowercaseCharacters = /[a-z]+/.test(value);
+    const hasNumber = /[0-9]+/.test(value);
+    const validPassword = hasUppercaseCharacters && hasLowercaseCharacters &&hasNumber;
+    return validPassword ? null : {invalidPassword: true};
+
+  }
+}
